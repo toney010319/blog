271fdb15791d491afcf277c54ef424809f6d9049
Toney010319 <toney010319@gmail.com>
2023-11-21 20:41:55 +0800

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_114405) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_123923) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
